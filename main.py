88d3d485157ce9262bc67e25c8c748a5a707f298
zerocola355ml <sin51611630@gmail.com>
2025-10-27 22:35:42 +0900

    Fix: Correct indentation in Config class

--- main.py
+++ main.py
@@ -82,8 +82,8 @@ class Config:
     
     # 브라우저 설정
     USE_PROFILE = False     # Chrome 프로필 사용
-CHROME_PROFILE_PATH = "C:\\Users\\tlsgj\\AppData\\Local\\Google\\Chrome\\User Data"
-PROFILE_DIRECTORY = "Default"
+    CHROME_PROFILE_PATH = "C:\\Users\\tlsgj\\AppData\\Local\\Google\\Chrome\\User Data"
+    PROFILE_DIRECTORY = "Default"
 
     # 페이지 로딩 설정
     PAGE_LOAD_WAIT = 15     # 페이지 로딩 대기 시간 (초)
